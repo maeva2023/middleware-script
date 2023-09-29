248507d77bd35942d8dc3c81fa4feaa19ee25d6e
root <root@ip-172-26-3-138.ec2.internal>
2023-09-29 11:44:37 +0000

    agent script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## This is a test script
+# created by : Serge or email user id
+# Date: sept 2023
+# Modified on nov 2030 by Eva
+
+
+
+
+sleep 2
+echo "hello world"    # This is displaying a message
+sleep 2
+echo "Welcome to our store dont forget to visit aisle 13 for our specials"
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if  [ -f /etc/passwdekkjjhh ]
+
+then
+ echo exist
+else
+ echo missing
+ fi
+
+if [  -d /tmp/serge1 ]
+ then
+  echo exist
+else
+ mkdir /tmp/serge1
+echo new > /tmp/serge1/file
+cat /tmp/serge1/file
+
+fi
